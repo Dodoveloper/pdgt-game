2ffe7db60ed8be1973cb8c0f905dd8644cd3d040
DodoIta <davide.cor94@gmail.com>
2018-03-18 22:57:12 +0100

    expanded grid, set up second tilemap

--- Scripts/GridVisualizer.gd
+++ Scripts/GridVisualizer.gd
@@ -1,20 +1,24 @@
-extends Node2D
+extends TileMap
 
 onready var screensize = get_viewport_rect().size
-onready var cell_size = 64
-onready var grid_size = Vector2(int(screensize.x / cell_size), int(ceil(screensize.y / cell_size)))
-onready var tile_size = Vector2(cell_size, cell_size)
+onready var grid_size = Vector2(36, 21) # number of cells
 
-#func _draw():
-#	var LINE_COLOR = Color(1, 1, 1)
-#	var LINE_WIDTH = 2
-#
-#	for x in range(0, grid_size.x + 1):
-#		var next_pos = x * tile_size.x
-#		var limit = grid_size.y * tile_size.y
-#		draw_line(Vector2(next_pos, 0), Vector2(next_pos, limit), LINE_COLOR, LINE_WIDTH)
-#
-#	for y in range(0, grid_size.y + 1):
-#		var next_pos = y * tile_size.y
-#		var limit = grid_size.x * tile_size.x
-#		draw_line(Vector2(0, next_pos), Vector2(limit, next_pos), LINE_COLOR, LINE_WIDTH)
+func _ready():
+	z_index = 10 # testing
+
+func _draw():
+	var LINE_COLOR = Color(1, 1, 1, 0.2)
+	var LINE_WIDTH = 2
+
+	# draw the columns
+	for x in range(-grid_size.x, grid_size.x + 1):
+		var next_pos = x * cell_size.x
+		var start_y = (-grid_size.y * cell_size.y) - 1
+		var limit = grid_size.y * cell_size.y
+		draw_line(Vector2(next_pos, start_y), Vector2(next_pos, limit), LINE_COLOR, LINE_WIDTH)
+	# draw the rows
+	for y in range(-grid_size.y, grid_size.y + 1):
+		var next_pos = y * cell_size.y
+		var start_x = -grid_size.x * cell_size.x
+		var limit = grid_size.x * cell_size.x
+		draw_line(Vector2(start_x, next_pos), Vector2(limit, next_pos), LINE_COLOR, LINE_WIDTH)
