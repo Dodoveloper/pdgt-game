895e93d12e9c15ee24991def3bc18decfd4afcbc
DodoIta <davide.cor94@gmail.com>
2018-03-30 14:48:09 +0200

    implemented some getters

--- Scripts/DataHandler.gd
+++ Scripts/DataHandler.gd
@@ -1,6 +1,7 @@
 extends Node
 
-const DATA_PATH = "res://Data/Elenco-piattaforme-marine"
+const DATA_PATH = "res://Data/Elenco-piattaforme-marine.json"
+var data
 
 func _ready():
 	# try to load the file
@@ -10,9 +11,50 @@ func _ready():
 		return
 	# read the file and store it into a dictionary
 	file.open(DATA_PATH, File.READ)
-	var data = {}
-	data.parse_json(file.get_as_text())
-	print(data)
+	var json = JSON.parse(file.get_as_text())
+	data = json.result
+#	for i in data.size():
+#		if "ADA" in data[i].cdenominazione__:
+#			print(data[i].cdenominazione__)
+	get_platform_pit("LUNA A")
 
-func read_size(name):
-	pass
+func get_platform_size(platform_name):
+	# get the right index
+	var index
+	for i in data.size():
+		if platform_name == data[i].cdenominazione__:
+			index = i
+			break
+	# get the size values
+	if data[index].cdimensioni == "" or data[index].cdimensioni == null:
+		print("The platform has no specified size")
+		return
+	else:
+		# get the string and convert it
+		var platform_size = data[index].cdimensioni
+		platform_size = platform_size.replacen("x", "")
+		platform_size = platform_size.replacen("m", "")
+		var n1 = platform_size.left(3).to_int()
+		var n2 = platform_size.right(3).to_int()
+		print(platform_size, " ", n1, " ", n2, " ", n1*n2)
+		return n1*n2
+
+func get_platform_pit(platform_name):
+	var index
+	for i in data.size():
+		if platform_name == data[i].cdenominazione__:
+			index = i
+			break
+	# get the value
+	if data[index].cpozzi_in_produzione == "" or data[index].cpozzi_in_produzione == null:
+		print("The platform has no specified pits")
+	else:
+		# get the string and convert it
+		var pit_count = data[index].cpozzi_in_produzione.to_int()
+		print(pit_count)
+		return pit_count
+
+
+
+
+
