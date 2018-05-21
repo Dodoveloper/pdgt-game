384020f4593375fdaf9bffa83d5caf9a319c747a
DodoIta <davide.cor94@gmail.com>
2018-05-21 16:35:58 +0200

    refined panel

--- game/Scripts/PlatformInfo.gd
+++ game/Scripts/PlatformInfo.gd
@@ -1,12 +1,16 @@
 extends NinePatchRect
 
 onready var infos = get_parent().info_dict
-
-func _ready():
-	pass # replace with function body
+var max_row = 0
 
 func _on_PlatformInfo_draw():
 	# display the text
 	for row in $Rows.get_children():
 		row.get_node("HBox/Label").text = row.name + ":"
 		row.get_node("HBox/Value").text = String(infos[row.name])
+		if row.rect_size.x > max_row:
+			max_row = row.rect_size.x + 100
+
+func _process(delta):
+	# update accordingly to children's size
+	rect_size.x = max_row
