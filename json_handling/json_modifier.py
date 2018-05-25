85b2e1786bcfc94a51f3c2c5a2aa6544755ececf
DodoIta <davide.cor94@gmail.com>
2018-05-26 01:59:33 +0200

    deleted platforms with less than 4 pits

--- json_handling/json_modifier.py
+++ json_handling/json_modifier.py
@@ -10,8 +10,6 @@ def pit_sum(platform):
         sum += int(platform["cpozzi_produttivi_non_eroganti"])
     if platform["cpozzi_in_produzione"] != "":
         sum += int(platform["cpozzi_in_produzione"])
-        if platform["cpozzi_in_monitoraggio"] != "":
-            sum += int(platform["cpozzi_in_monitoraggio"])
     return sum
 
 # start modifying the json
@@ -21,11 +19,17 @@ with open(input_json, 'r') as f:
 
 # iterate through each element of the array
 for i, val in enumerate(data):
+    print(pit_sum(data[i]), data[i]["cnumero_pozzi_allacciati__"])
     # iterate through the elements of each dict
     for k, v in list(data[i].items()):
         if k == "ctitolo_minerario" or k == "cfoglio":
             del data[i][k]
-    print(pit_sum(data[i]))
+    # delete platforms with few pits
+    if int(data[i]["cnumero_pozzi_allacciati__"]) < 4:
+        print("Deleting %s with %s total pits" % (data[i]["cdenominazione__"], data[i]["cnumero_pozzi_allacciati__"]))
+        del data[i]
+
+print(len(data))
 
 with open(input_json, 'w') as f:
     json.dump(data, f)
