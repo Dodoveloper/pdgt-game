340584bf0eef490f17fd81054871f7d01159308d
DodoIta <davide.cor94@gmail.com>
2018-06-26 17:33:51 +0200

    fixed writing to JSON

--- game/Scripts/WeatherRequest.gd
+++ game/Scripts/WeatherRequest.gd
@@ -14,31 +14,21 @@ func _ready():
 			print("OWM data absent!")
 			return
 		# read the file and store it into an array of dictionaries
-		file.open(DATA_PATH, File.READ_WRITE)
+		file.open(DATA_PATH, File.READ)
 		var json = JSON.parse(file.get_as_text())
 		data = json.result
 		Global.my_token = data.owm_token
+		# close the file
+		file.close()
 	# check if it's necessary another request based on time
 	if check_time():
 		platform.connect("platform_initialized", self, "make_request")
 
 func make_request(location):
-	var cur_datetime = OS.get_datetime()
-	# write current time in JSON
-	data["owm_token"] = Global.my_token
-	data["last_req"].year = str(cur_datetime.year)
-	data["last_req"].month = str(cur_datetime.month)
-	data["last_req"].day = str(cur_datetime.day)
-	data["last_req"].hour = str(cur_datetime.hour)
-	data["last_req"].minute = str(cur_datetime.minute)
-
-	#file.store_string(to_json(data))
 	var params = [str(location.x), str(location.y), str(Global.my_token)]
-	var req = "api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric&APPID=%s" % params
+	var req = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric&appid=%s" % params
 	print(req)
-	print($HTTPRequest.request(req))
-	# close the file
-	file.close()
+	$HTTPRequest.request(req)
 
 func check_time():
 	var cur_datetime = OS.get_datetime()
@@ -67,10 +57,24 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if response_code != 0:
 		printerr("bad request")
 		return
-	
+
+	write_last_request()
 	var json = JSON.parse(body.get_string_from_utf8())
 	print(json.result)
 
+func write_last_request():
+	var cur_datetime = OS.get_datetime()
+	# write current time in JSON
+	data["owm_token"] = Global.my_token
+	data["last_req"].year = str(cur_datetime.year)
+	data["last_req"].month = str(cur_datetime.month)
+	data["last_req"].day = str(cur_datetime.day)
+	data["last_req"].hour = str(cur_datetime.hour)
+	data["last_req"].minute = str(cur_datetime.minute)
+	file = File.new()
+	file.open(DATA_PATH, File.WRITE)
+	file.store_string(JSON.print(data, "  ", true))
+	file.close()
 
 
 
