22ae1a62faa6991a9d1abefa525ef98548873682
DodoIta <davide.cor94@gmail.com>
2018-07-21 21:28:38 +0200

    added some buttons and platform info

--- game/Scripts/PlatformInfo.gd
+++ game/Scripts/PlatformInfo.gd
@@ -7,10 +7,4 @@ func _on_PlatformInfo_draw():
 	# display the text
 	for row in $Rows.get_children():
 		row.get_node("HBox/Label").text = row.name + ":"
-		row.get_node("HBox/Value").text = String(infos[row.name])
-		if row.rect_size.x > max_row:
-			max_row = row.rect_size.x + 100
-
-func _process(delta):
-	# update accordingly to children's size
-	rect_size.x = max_row
+#		row.get_node("HBox/Value").text = String(infos[row.name]) # UNCOMMENT
